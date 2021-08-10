6b895faf7f72af777d27656f7372f2df7948df4c
Chris K <Chris-SourceCode@outlook.com>
2021-08-10 22:46:09 +0100

    feat: Add new routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -23,6 +25,18 @@ app.post('/register', (req, res) => {
 });
 
 app.post('/signin', signIn.handleSignIn(db, bcrypt));
+
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
